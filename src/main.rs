b999ed3c3701cb4c26acf54e0104cbbb3d73b437
Johannes Sarpola <johannes.sarpola@gmail.com>
2024-04-16 16:59:27 +0300

    added basic structs

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,36 @@ fn gather_files(path: &str) -> Result<Vec<PathBuf>, io::Error> {
     Ok(files)
 }
 
+struct ChunkyBoyoConfig {
+    executor_size: u32,
+    chunk_size: u32,
+    output_folder: String,
+    input_folder: String,
+    separator: u8,
+    verbose: bool,
+}
+
+struct ReaderBoyo {
+    config: ChunkyBoyoConfig,
+}
+
+struct SplitterBoyo {
+    chunk_size: u32,
+    input_path: String,
+    executor_size: u32,
+    separator: u8,
+}
+
+struct ProgressBoyo {
+
+}
+
+struct WriterBoyo {
+    output_path: String,
+    source: Vec<Vec<u8>>,
+    progress_boyo: ProgressBoyo,
+}
+
 fn main() -> anyhow::Result<()> {
     println!("Hello, world!");
     let files = gather_files(".in")?;
